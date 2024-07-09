4a441b8a19a47a0b2385c9d02b7855b514f1d8c3
Ramon Elói Magalhães de Jesus <120483254+REloi10@users.noreply.github.com>
2024-07-08 23:09:43 -0300

    Update tabuleiro.c

--- tabuleiro.c
+++ tabuleiro.c
@@ -2,8 +2,35 @@
 #include <stdlib.h>
 #include "tabuleiro.h"
 void Inicializa_tabuleiro(tabuleiro *t){
-    t->primeiro = (Apontador) malloc(sizeof(localidade));
+    t->primeiro = (Apontador) malloc(sizeof(Celula));
     t->ultimo = t->primeiro;
     t->primeiro->prox = NULL;
     t->tamanho = 0;
 }
+
+void Insere_Localidade(tabuleiro *t, localidade l){
+    t->primeiro->prox =(Celula*) malloc(sizeof(Celula));
+    t->ultimo = t->primeiro->prox;
+    t->ultimo->local = l;
+    t->ultimo->prox = NULL;
+    t->tamanho ++;
+}
+
+localidade Avanca_Casas(tabuleiro *t, int Casas){
+    Apontador aux = t->primeiro;
+    for (int i = 0; i <Casas; i++) {
+        aux = aux->prox;
+    }
+    return aux->local;
+
+}
+
+
+void Imprime_Tabuleiro(tabuleiro t){
+    Celula *aux = t.primeiro->prox;
+    for(int i=0;i<t.tamanho;i++){
+        printf("%s", aux->local.endereco);
+        aux = aux->prox;
+    }
+}
+
