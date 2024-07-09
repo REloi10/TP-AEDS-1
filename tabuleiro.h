5a593e3d54391e62c7fb7c7ca5317b6f5b778db5
Ramon Elói Magalhães de Jesus <120483254+REloi10@users.noreply.github.com>
2024-07-08 23:08:41 -0300

    Update tabuleiro.h

--- tabuleiro.h
+++ tabuleiro.h
@@ -1,8 +1,6 @@
 #ifndef TABULEIRO_H_INCLUDED
 #define TABULEIRO_H_INCLUDED
 #include "localidade.h"
-typedef localidade* Apontador;
-
 typedef struct{
     Apontador primeiro;
     Apontador ultimo;
@@ -10,5 +8,7 @@ typedef struct{
 }tabuleiro;
 
 void Inicializa_tabuleiro(tabuleiro *t);
-
+void Insere_Localidade(tabuleiro *t, localidade l);
+localidade Avanca_Casas(tabuleiro *t, int Casas);
+void Imprime_Tabuleiro(tabuleiro t);
 #endif // TABULEIRO_H_INCLUDED
