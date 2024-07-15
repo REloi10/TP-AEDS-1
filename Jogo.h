8529bb0a79afcb3cef14ac3fc194f7f3f6475b78
Ramon Elói Magalhães de Jesus <120483254+REloi10@users.noreply.github.com>
2024-07-15 17:57:48 -0300

    Update Jogo.h

--- Jogo.h
+++ Jogo.h
@@ -1,6 +1,10 @@
 #ifndef JOGO_H_INCLUDED
 #define JOGO_H_INCLUDED
+#include "tabuleiro.h"
+typedef struct{
 
+}Jogo;
+void preenche_tabuleiro(FILE *arq, tabuleiro *t);
 
 
 #endif // JOGO_H_INCLUDED
