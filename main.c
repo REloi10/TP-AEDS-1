481b209f4cf07aac64e70d9d0c957a7700147dd4
Ramon Elói Magalhães de Jesus <120483254+REloi10@users.noreply.github.com>
2024-07-15 17:50:48 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <locale.h>
+#include "jogo.h"
 int main()
 {
-
+    setlocale(LC_ALL, "Portuguese");
     printf("Bem vindo ao Monopoly\n");
+    tabuleiro t;
+    Inicializa_tabuleiro(&t);
 
+    preenche_tabuleiro("localidades.txt",&t);
+    Imprime_Tabuleiro(&t);
     return 0;
 }
